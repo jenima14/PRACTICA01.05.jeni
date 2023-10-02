0e32db95816127c746f44101169e5aac3fa38076
jenima14 <jmarticarr2@educacion.navarra.es>
2023-10-02 12:13:57 +0200

    ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+Peso_Payaso = 112
+Peso_Muñeca = 75
+Payasos = int(input("Introducir número de payasos a enviar: "))
+Muñecas = int(input("Introducir número de muñecas a enviar: "))
+Peso_Total = Peso_Payaso * Payasos + Peso_Muñeca * Muñecas
+print("El peso total del paquete es " + str(Peso_Total))
+input()
