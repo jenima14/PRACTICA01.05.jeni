316de463787eb5ed217e8ca809b63ad34466d5df
jenima14 <jmarticarr2@educacion.navarra.es>
2023-10-02 12:06:19 +0200

    ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,6 @@
+n = int(input("Introduce el dividendo (n): "))
+m = int(input("Introduce el divisor (m): "))
+c = n // m
+r = n % m
+print(n, "entre", m, "da un cociente", c, "y un resto", r)
+input()
